0712a7c541e2b704a20b5cbcdd8400ea9efc0877
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2019-06-24 02:45:17 +0200

    Added Web Tempalte

---
+++ precache-manifest.801157064b49cc21cdf9ef7ffd63f1a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d48646951267d70bba1e4849904f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9fa95ca76e8ef8215d",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f90fe2083c7e7bfa0e46",
+    "url": "/static/css/main.6cd8c992.chunk.css"
+  },
+  {
+    "revision": "4e9fa95ca76e8ef8215d",
+    "url": "/static/js/2.dff01fa1.chunk.js"
+  },
+  {
+    "revision": "f90fe2083c7e7bfa0e46",
+    "url": "/static/js/main.94b271fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
